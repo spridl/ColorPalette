2badc91a3a81b6bfb283a3823467742d50378894
nasim <nasim_nozirov@mail.ru>
2022-03-08 19:14:30 +0300

    total

--- ColorPalette/ViewController.swift
+++ ColorPalette/ViewController.swift
@@ -21,31 +21,50 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        screenView.layer.cornerRadius = screenView.frame.width / 60
-        screenView.backgroundColor = UIColor(
-            red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value),
-            blue: CGFloat(blueSlider.value), alpha: 1
-            )
+        screenView.layer.cornerRadius = 15
+        setColor()
+       setValue(for: displayRedLabel, displayGreenLabel, displayBlueLabel)
     }
     
-    @IBAction func colorSetting() {
-        settingLabel()
+    @IBAction func colorSetting(_ sender: UISlider) {
+      setColor()
+        switch sender {
+        case redSlider:
+            displayRedLabel.text = string(from: redSlider)
+        case greenSlider:
+            displayGreenLabel.text = string(from: greenSlider)
+        default:
+            displayBlueLabel.text = string(from: blueSlider)
+        }
+    }
     
+    private func setColor() {
         screenView.backgroundColor = UIColor(
             red: CGFloat(redSlider.value),
             green: CGFloat(greenSlider.value),
             blue: CGFloat(blueSlider.value),
-            alpha: 1
+            alpha: 1.0
         )
     }
     
-    private func settingLabel() {
-        displayRedLabel.text = String(format: "%.2f", redSlider.value)
-        displayGreenLabel.text = String(format: "%.2f", greenSlider.value)
-        displayBlueLabel.text = String(format: "%.2f", blueSlider.value)
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label  {
+            case displayRedLabel:
+                displayRedLabel.text = string(from: redSlider)
+            case displayGreenLabel:
+                displayGreenLabel.text = string(from: greenSlider)
+            default:
+                displayBlueLabel.text = string(from: blueSlider)
+            }
+        }
     }
     
+    private func string(from slider: UISlider ) -> String {
+        String(format: "%.2f", slider.value)
+    }
 }
 
-
+//blueSlider.value = Float.random(in: 0...1)
+//greenSlider.value = Float.random(in: 0...1)
+//blueSlider.value = Float.random(in: 0...1)
